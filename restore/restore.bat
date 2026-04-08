352534a6893a8b49d641bea42ad15acf74d08d01
Alvin Dancy <devops@users.noreply.github.com>
2026-04-07 21:55:31 -0500

    Fixed Restore batch Base Dir

--- restore/restore.bat
+++ restore/restore.bat
@@ -9,7 +9,7 @@ echo.
 :: =========================
 :: BASE PATH (LOCKED TO YOUR STRUCTURE)
 :: =========================
-set "BASE_DIR=C:\%USERPROFILE%\Bookstack"
+set "BASE_DIR=%USERPROFILE%\Bookstack"
 set "BACKUP_BASE_DIR=%BASE_DIR%\backup\backups"
 
 set "DB_CONTAINER=mariadb"
